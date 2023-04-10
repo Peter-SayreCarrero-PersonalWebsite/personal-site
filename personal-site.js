7e17f32e297e9fef84ab5065c42e870a603087bf
zyndicate <psayre@live.com>
2023-04-09 20:48:10 -0400

    fricking work

---
+++ personal-site.js
@@ -0,0 +1,4 @@
+// What exactly do I need to put here?
+
+const whyeven = `Why are we here? Just to suffer? It must be really cool that only you can read this specific message here. Congrats, you inspected this page's element and clicked onto the console.`
+console.log(whyeven)
